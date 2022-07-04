c5727b567203f442bef869852d71c72af1e56749
Kevin Clark <kevin.clark@gmail.com>
2022-07-04 14:24:08 -0700

    Extract ultrasonic_sensor and friends

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@ use std::sync::Arc;
 use std::time::*;
 use core::fmt::Write;
 
-use embedded_hal::{
-    prelude::*,
-    digital::v2::*,
-    blocking::delay::DelayUs
-};
+use embedded_hal::prelude::*;
 
 use esp_idf_hal::{
     prelude::*,
@@ -38,10 +34,13 @@ use esp_idf_sys::EspError;
 
 use anyhow::{bail, Result};
 use generic_array::typenum::U5;
-use heapless::spsc::{Queue, Consumer};
+use heapless::spsc::Queue;
 use log::*;
 use median::stack::Filter;
 use ssd1306::mode::DisplayConfig;
+
+use greywater::ultrasonic_sensor;
+
 
 const SSID: &str = env!("GREYWATER_WIFI_SSID");
 const PASS: &str = env!("GREYWATER_WIFI_PASS");
@@ -149,69 +148,6 @@ fn loop_forever() -> Result<()> {
     loop { }
 
     Ok(())
-}
-
-// This is necessarily a macro because the pins are different *types*
-// with no common traits that would let me write a more generic helper
-// function or struct. So that's cool...
-#[macro_export]
-macro_rules! ultrasonic_sensor {
-    ($trigger_pin:expr, $echo_pin:expr, $queue:expr) => {
-        {
-            let (mut tx, response) = unsafe { $queue.split() };
-
-            let trigger_pin = $trigger_pin
-                .into_output()
-                .expect("Setting trigger pin as output")
-                .degrade();
-
-            let echo_pin = $echo_pin
-                .into_input()
-                .expect("Setting echo pin as input")
-                .into_pull_down()
-                .expect("Enabling echo pin pull down");
-
-            unsafe {
-                echo_pin.into_subscribed(move ||{
-                    let now = EspSystemTime {}.now();
-                    tx.enqueue(now).expect("Enqueuing time");
-                }, InterruptType::AnyEdge)
-                    .expect("Setting edge interrupt for echo pin");
-            }
-
-            UltrasonicSensor { trigger_pin, response }
-        }
-    };
-}
-
-struct UltrasonicSensor {
-    trigger_pin: GpioPin<Output>,
-    response: Consumer<'static, Duration, 2>
-}
-
-impl UltrasonicSensor {
-    fn distance_in_cms(&mut self) -> f32 {
-        debug!("Starting trigger pulse");
-        self.trigger_pin.set_high().expect("Starting trigger pulse");
-        delay::Ets.delay_us(10u8);
-        self.trigger_pin.set_low().expect("Ending trigger pulse");
-        debug!("Pulse done.");
-
-        let mut blocking_dequeue = move || {
-            while !self.response.ready() {}
-            unsafe { self.response.dequeue_unchecked() }
-        };
-
-        let start = blocking_dequeue();
-        debug!("Got start: {:?}", start);
-        let end = blocking_dequeue();
-        debug!("Got end: {:?}", end);
-
-        let raw = (end - start).as_micros() as f32 / 58.0;
-        debug!("Raw: {}", raw);
-
-        raw
-    }
 }
 
 #[allow(dead_code)] // The listener_handle just needs to hold the thread reference
---
+++ src/sensors.rs
@@ -0,0 +1,82 @@
+use std::time::Duration;
+
+use embedded_hal::{
+    digital::v2::*,
+    blocking::delay::DelayUs
+};
+
+use esp_idf_hal::{
+    gpio::*,
+    delay
+};
+
+use log::*;
+use heapless::spsc::Consumer;
+
+// This is necessarily a macro because the pins are different *types*
+// with no common traits that would let me write a more generic helper
+// function or struct. So that's cool...
+#[macro_export]
+macro_rules! ultrasonic_sensor {
+    ($trigger_pin:expr, $echo_pin:expr, $queue:expr) => {
+        {
+            let (mut tx, response) = unsafe { $queue.split() };
+
+            let trigger_pin = $trigger_pin
+                .into_output()
+                .expect("Setting trigger pin as output")
+                .degrade();
+
+            let echo_pin = $echo_pin
+                .into_input()
+                .expect("Setting echo pin as input")
+                .into_pull_down()
+                .expect("Enabling echo pin pull down");
+
+            unsafe {
+                echo_pin.into_subscribed(move ||{
+                    let now = EspSystemTime {}.now();
+                    tx.enqueue(now).expect("Enqueuing time");
+                }, InterruptType::AnyEdge)
+                    .expect("Setting edge interrupt for echo pin");
+            }
+
+            $crate::sensors::UltrasonicSensor::new(trigger_pin, response)
+        }
+    };
+}
+
+pub struct UltrasonicSensor {
+    trigger_pin: GpioPin<Output>,
+    response: Consumer<'static, Duration, 2>
+}
+
+impl UltrasonicSensor {
+    pub fn new(trigger_pin: GpioPin<Output>, response: Consumer<'static, Duration, 2>) -> UltrasonicSensor {
+        UltrasonicSensor { trigger_pin, response }
+    }
+
+    pub fn distance_in_cms(&mut self) -> f32 {
+        debug!("Starting trigger pulse");
+        self.trigger_pin.set_high().expect("Starting trigger pulse");
+        delay::Ets.delay_us(10u8);
+        self.trigger_pin.set_low().expect("Ending trigger pulse");
+        debug!("Pulse done.");
+
+        let mut blocking_dequeue = move || {
+            while !self.response.ready() {}
+            unsafe { self.response.dequeue_unchecked() }
+        };
+
+        let start = blocking_dequeue();
+        debug!("Got start: {:?}", start);
+        let end = blocking_dequeue();
+        debug!("Got end: {:?}", end);
+
+        let raw = (end - start).as_micros() as f32 / 58.0;
+        debug!("Raw: {}", raw);
+
+        raw
+    }
+}
+
